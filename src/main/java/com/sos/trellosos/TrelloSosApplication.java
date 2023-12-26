650416ca89de7e9e6a7d909cf72e24836827d877
baegyutae <141345981+baegyutae@users.noreply.github.com>
2023-12-26 14:38:28 +0900

    Add files via upload

---
+++ src/main/java/com/sos/trellosos/TrelloSosApplication.java
@@ -0,0 +1,13 @@
+package com.sos.trellosos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrelloSosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrelloSosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sos/trellosos/TrelloSosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sos.trellosos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrelloSosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
