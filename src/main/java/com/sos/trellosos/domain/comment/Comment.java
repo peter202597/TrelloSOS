bfd5f2e88db220eaac978a658c6e17e859a1f8be
Sk1palong <cchs33@gmail.com>
2023-12-27 10:46:02 +0900

    feat : Card 연관관계 설정

--- src/main/java/com/sos/trellosos/domain/comment/Comment.java
+++ src/main/java/com/sos/trellosos/domain/comment/Comment.java
@@ -1,9 +1,13 @@
 package com.sos.trellosos.domain.comment;
 
+import com.sos.trellosos.domain.card.Card;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,4 +24,7 @@ public class Comment {
     private Long id;
     private String comment;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "card_id")
+    private Card card;
 }
