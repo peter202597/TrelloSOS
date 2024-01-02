16864fd544df20be30fab29e662f0f3f8b5f1cb3
peter202597 <peter202597@gmail.com>
2024-01-02 10:00:07 +0900

    refactor : 미사용 코드 제거

--- src/main/java/com/sos/trellosos/domain/column/dto/ColumnResponseDto.java
+++ src/main/java/com/sos/trellosos/domain/column/dto/ColumnResponseDto.java
@@ -2,10 +2,7 @@ package com.sos.trellosos.domain.column.dto;
 
 import com.sos.trellosos.domain.column.entity.Columns;
 import com.sos.trellosos.global.dto.CommonResponseDto;
-import lombok.AccessLevel;
-import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 //컬럼 생성 응답 DTO
 @Getter
